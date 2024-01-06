687d378891b884c095436a57a2b9464f3d48bf45
mansao01 <novrilukman15@gmail.com>
2024-01-06 17:34:44 +0700

    add splash screen and improve statusBar and navigationBar color to transparent

--- app/src/main/java/com/mansao/trianglesneacare/MainActivity.kt
+++ app/src/main/java/com/mansao/trianglesneacare/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.mansao.trianglesneacare
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mansao.trianglesneacare.ui.AuthViewModel
 import com.mansao.trianglesneacare.ui.TriangleApp
 import com.mansao.trianglesneacare.ui.theme.TriangleSneacareTheme
@@ -18,8 +22,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val authViewModel by viewModels<AuthViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
         setContent {
+            installSplashScreen().setKeepOnScreenCondition { !authViewModel.isLoading.value }
             TriangleSneacareTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
