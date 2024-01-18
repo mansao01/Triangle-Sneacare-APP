5e4e8cd0f5e7f0e7207580e6bc74659f8ab7aee6
mansao01 <novrilukman15@gmail.com>
2024-01-18 08:18:11 +0700

    working at profile Screen, but i forgot to add image in profile endpoint

--- app/src/main/java/com/mansao/trianglesneacare/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/mansao/trianglesneacare/ui/screen/profile/ProfileScreen.kt
@@ -2,14 +2,20 @@ package com.mansao.trianglesneacare.ui.screen.profile
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.mansao.trianglesneacare.R
 import com.mansao.trianglesneacare.data.network.response.ProfileResponse
 import com.mansao.trianglesneacare.ui.common.ProfileUiState
@@ -38,9 +44,37 @@ fun ProfileComponent(
     viewModel: ProfileViewModel
 ) {
     Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = profile.toString())
+        UserDetailComponent(image = profile.profile.name, name =profile.profile.name , email = profile.profile.email)
         Button(onClick = { viewModel.logout() }) {
             Text(text = stringResource(R.string.logout))
         }
     }
+}
+
+@Composable
+fun UserDetailComponent(
+    image:String,
+    name:String,
+    email:String
+) {
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(CircleShape),
+            model = ImageRequest.Builder(context)
+                .crossfade(true)
+                .data(image)
+                .build(),
+            contentDescription = null
+        )
+        Column {
+            Text(text = name)
+            Text(text = email)
+        }
+    }
+
 }
