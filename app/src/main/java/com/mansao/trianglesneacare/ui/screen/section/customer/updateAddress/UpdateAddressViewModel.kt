e55071ea834241608d7bb2fe8860dcfb1eb4f4fa
mansao01 <novrilukman15@gmail.com>
2024-02-09 19:50:52 +0700

    create update address and delete address function in viewModel

--- app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/updateAddress/UpdateAddressViewModel.kt
+++ app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/updateAddress/UpdateAddressViewModel.kt
@@ -19,6 +19,10 @@ class UpdateAddressViewModel @Inject constructor(private val appRepositoryImpl: 
         MutableStateFlow(UiState.Standby)
     val uiState: StateFlow<UiState<OnlyMsgResponse>> = _uiState
 
+    private var _deleteMessage: MutableStateFlow<String> =
+        MutableStateFlow("")
+    val deleteMessage: StateFlow<String> = _deleteMessage
+
     fun updateAddress(
         id: Int,
         receiverName: String,
@@ -28,7 +32,13 @@ class UpdateAddressViewModel @Inject constructor(private val appRepositoryImpl: 
         viewModelScope.launch {
             try {
                 val token = appRepositoryImpl.getAccessToken()
-                val result = appRepositoryImpl.updateCustomerAddress("Bearer $token", id, receiverName, fullAddress, note)
+                val result = appRepositoryImpl.updateCustomerAddress(
+                    "Bearer $token",
+                    id,
+                    receiverName,
+                    fullAddress,
+                    note
+                )
                 _uiState.value = UiState.Success(result)
             } catch (e: Exception) {
                 _uiState.value = UiState.Error(e.message.toString())
@@ -37,12 +47,14 @@ class UpdateAddressViewModel @Inject constructor(private val appRepositoryImpl: 
         }
     }
 
-    fun deleteAddress() {
+    fun deleteAddress(id: Int) {
         viewModelScope.launch {
             try {
-
+                val token = appRepositoryImpl.getAccessToken()
+                val result = appRepositoryImpl.deleteCustomerAddress("Bearer $token", id)
+                _deleteMessage.value = result.msg
             } catch (e: Exception) {
-
+                _deleteMessage.value = e.message.toString()
             }
         }
     }
