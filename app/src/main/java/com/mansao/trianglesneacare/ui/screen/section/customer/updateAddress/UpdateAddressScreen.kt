f5ff93790142cd7ec5efeaffe997acfc4d0eca03
mansao01 <novrilukman15@gmail.com>
2024-02-15 20:12:25 +0700

    add uiState in UpdateAddressScreen

--- app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/updateAddress/UpdateAddressScreen.kt
+++ app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/updateAddress/UpdateAddressScreen.kt
@@ -1,15 +1,34 @@
 package com.mansao.trianglesneacare.ui.screen.section.customer.updateAddress
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mansao.trianglesneacare.data.network.response.dto.Address
+import com.mansao.trianglesneacare.ui.common.UiState
+import com.mansao.trianglesneacare.ui.components.LoadingDialog
 
 @Composable
-fun UpdateAddressScreen() {
-    
+fun UpdateAddressScreen(
+    updateAddressViewModel: UpdateAddressViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
+    updateAddressViewModel.uiState.collectAsState(initial = UiState.Standby).value.let { uiState ->
+        when (uiState) {
+            is UiState.Standby -> {}
+            is UiState.Loading -> LoadingDialog()
+            is UiState.Success -> UpdateAddressComponent(address = uiState.data.address)
+            is UiState.Error -> Toast.makeText(context, uiState.errorMessage, Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
 }
 
 @Composable
 fun UpdateAddressComponent(
-
+    address: Address
 ) {
 
 }
