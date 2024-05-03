f2ae40ed93a411d58b5a133a37dcd95193c3415d
mansao01 <novrilukman15@gmail.com>
2024-05-03 10:21:18 +0700

    working at customer home screen

--- app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/home/CustomerHomeScreen.kt
+++ app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/home/CustomerHomeScreen.kt
@@ -1,12 +1,88 @@
 package com.mansao.trianglesneacare.ui.screen.section.customer.home
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mansao.trianglesneacare.data.network.response.CategoriesItem
+import com.mansao.trianglesneacare.ui.common.UiState
+import com.mansao.trianglesneacare.ui.components.LoadingDialog
 
 @Composable
-fun CustomerHomeScreen() {
+fun CustomerHomeScreen(
+    customerHomeViewModel: CustomerHomeViewModel = hiltViewModel()
+) {
+    LaunchedEffect(Unit) {
+        customerHomeViewModel.getCategories()
+    }
+    val username = customerHomeViewModel.username.collectAsState(initial = "").value
+    customerHomeViewModel.uiState.collectAsState(initial = UiState.Standby).value.let { uiState ->
+        val context = LocalContext.current
+        when (uiState) {
+            UiState.Standby -> {}
+            UiState.Loading -> LoadingDialog()
+            is UiState.Error -> Toast.makeText(context, uiState.errorMessage, Toast.LENGTH_SHORT)
+                .show()
+
+            is UiState.Success -> CustomerHomeContent(
+                username = username,
+                categoryList = uiState.data.categories
+            )
+        }
+
+    }
+}
+
+@Composable
+fun CustomerHomeContent(
+    username: String,
+    categoryList: List<CategoriesItem>
+) {
+    Column {
+        CustomerHomeHeader(username = username)
+        CategoriesList(categoryList = categoryList)
+    }
+}
+
+@Composable
+fun CategoriesList(
+    categoryList: List<CategoriesItem>
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(128.dp),
+
+        // content padding
+        contentPadding = PaddingValues(
+            start = 12.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        )
+    ) {
+        items(categoryList.size) { index ->
+            Text(text = categoryList[index].itemType)
+        }
+    }
+
+}
+
+@Composable
+fun CustomerHomeHeader(
+    username: String
+) {
+    Text(text = "Welcome, $username!")
+
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/home/CustomerHomeViewModel.kt
+++ app/src/main/java/com/mansao/trianglesneacare/ui/screen/section/customer/home/CustomerHomeViewModel.kt
@@ -3,7 +3,7 @@ package com.mansao.trianglesneacare.ui.screen.section.customer.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mansao.trianglesneacare.data.AppRepositoryImpl
-import com.mansao.trianglesneacare.data.network.response.GetServicesByCategoryIdResponse
+import com.mansao.trianglesneacare.data.network.response.GetCategoriesResponse
 import com.mansao.trianglesneacare.ui.common.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -14,20 +14,34 @@ import javax.inject.Inject
 @HiltViewModel
 class CustomerHomeViewModel @Inject constructor(private val appRepositoryImpl: AppRepositoryImpl) :
     ViewModel() {
-    private var _uiState: MutableStateFlow<UiState<GetServicesByCategoryIdResponse>> =
+    private var _uiState: MutableStateFlow<UiState<GetCategoriesResponse>> =
         MutableStateFlow(UiState.Standby)
-    val uiState: Flow<UiState<GetServicesByCategoryIdResponse>> = _uiState
+    val uiState: Flow<UiState<GetCategoriesResponse>> = _uiState
+    private var _username: MutableStateFlow<String> = MutableStateFlow("")
+    val username: Flow<String> = _username
+
+    init {
+        getCustomerName()
+    }
     private fun setLoadingState() {
         _uiState.value = UiState.Loading
 
     }
-    fun getServicesByCategoryId(categoryId:Int) = viewModelScope.launch {
+
+    fun getCategories() = viewModelScope.launch {
         setLoadingState()
         try {
-            val result = appRepositoryImpl.getServicesByCategory(categoryId)
+            val result = appRepositoryImpl.getCategories()
             _uiState.value = UiState.Success(result)
         } catch (e: Exception) {
             _uiState.value = UiState.Error(e.message.toString())
+        }
+    }
+
+    private fun getCustomerName() = viewModelScope.launch {
+        val result = appRepositoryImpl.getUsername()
+        result?.let {
+            _username.value = it
         }
     }
 }
